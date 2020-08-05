7a57fc2a849c5c250b828f1eeb31c245ab5e70a4
juansecorrea12 <juansecorrea12@gmail.com>
2020-08-05 15:45:17 -0500

    build the project

---
+++ build/precache-manifest.040f1d19985eded673268a6a8a1754ba.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa00bf24b94efd70d0805862ed3a8102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb538c3117547a8e5df",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "2940c209e4c045d24e99",
+    "url": "/static/css/main.d822eff5.chunk.css"
+  },
+  {
+    "revision": "cfb538c3117547a8e5df",
+    "url": "/static/js/2.ac3e5f4e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ac3e5f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2940c209e4c045d24e99",
+    "url": "/static/js/main.e2ce3446.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  },
+  {
+    "revision": "59b07af019489037252b20e6c8142961",
+    "url": "/static/media/Blastoise.59b07af0.png"
+  },
+  {
+    "revision": "3af9ed7a69f56b91d86f67075bb92549",
+    "url": "/static/media/Bulbasaur.3af9ed7a.png"
+  },
+  {
+    "revision": "6cbbfee719d504f952ea855121792c6b",
+    "url": "/static/media/Butterfree.6cbbfee7.png"
+  },
+  {
+    "revision": "acc5959ff62d609b115ed3ee289d5cca",
+    "url": "/static/media/Caterpie.acc5959f.png"
+  },
+  {
+    "revision": "2ca3432a7df9f780aedf35577ee3177b",
+    "url": "/static/media/Charizard.2ca3432a.png"
+  },
+  {
+    "revision": "4690e3ab94d0d45ab73068d9a94031b9",
+    "url": "/static/media/Charmander.4690e3ab.png"
+  },
+  {
+    "revision": "c07ad1e0edc4a5ea3de14d31f373a615",
+    "url": "/static/media/Charmeleon.c07ad1e0.png"
+  },
+  {
+    "revision": "6676c1407d3f40a9ebb40ae958a39161",
+    "url": "/static/media/Ivysaur.6676c140.png"
+  },
+  {
+    "revision": "87302a821bd62ed14e7e36e6ac5bff29",
+    "url": "/static/media/Kakuna.87302a82.png"
+  },
+  {
+    "revision": "1da54ec192e0601625135260879e5c30",
+    "url": "/static/media/Metapod.1da54ec1.png"
+  },
+  {
+    "revision": "4846a8818810c09be194010527df9757",
+    "url": "/static/media/Pidgeot.4846a881.png"
+  },
+  {
+    "revision": "b003485675034bfb6eb9db4156b3000d",
+    "url": "/static/media/Pidgeotto.b0034856.png"
+  },
+  {
+    "revision": "ec3b7917e5258606d1932fe4ab84ab45",
+    "url": "/static/media/Pidgey.ec3b7917.png"
+  },
+  {
+    "revision": "72f71ea43b8b33db6c6d576958593c27",
+    "url": "/static/media/Raticate.72f71ea4.png"
+  },
+  {
+    "revision": "5dfb3298580bd15839cb04630f17ba42",
+    "url": "/static/media/Rattata.5dfb3298.png"
+  },
+  {
+    "revision": "b7b81ed6197db0e9cc20b1ce038ca2f6",
+    "url": "/static/media/Squirtle.b7b81ed6.png"
+  },
+  {
+    "revision": "290b1615f404a1938d465ff5a45bf509",
+    "url": "/static/media/Venusaur.290b1615.png"
+  },
+  {
+    "revision": "2dae2d15f28d35c16480b150c5cfa84e",
+    "url": "/static/media/Wartortle.2dae2d15.png"
+  },
+  {
+    "revision": "0cf872c4cd937cc30341100d9a1a5489",
+    "url": "/static/media/Weedlw.0cf872c4.png"
+  },
+  {
+    "revision": "678f059c2bf24fa5a7fb1ebda0d8d3b7",
+    "url": "/static/media/beedrill.678f059c.png"
+  }
+]);
